f0e2409e3cd7c518811dfa1ac66091dc51f77f45
Macarena Concha <maconcha4@uc.cl>
2021-04-05 12:49:41 -0400

    cambiado index

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const { play_game } = require('./src/play_game');
 const { is_valid_players, change_input } = require('./src/check_inputs');
 const prompt = require('prompt-sync')({ sigint: true });
 
-const ask_for_players = () => {
+const start_game = () => {
     /* Pregunta por los jugadores
     Retorna la función que inicializa el juego si los inputs son válidos, si no vuelve a preguntar. */
     const players = change_input(0)(prompt('Ingrese el nombre de los jugadores: '));
     return is_valid_players(players) ? play_game(players) : (console.log("Formato inválido."), ask_for_players());
 }
 
-ask_for_players()
+start_game();
